6f82b440b4e8c7ac87773cabc196545954135cda
anzarthewater <chromiehommie69@gmail.com>
2025-03-21 23:05:56 -0400

    -

---
+++ 3-JS/script.js
@@ -0,0 +1,26 @@
+var backgroundColor = "#0000FF";
+const headerColor = "#00FF00";
+let paragraphColor = "#FF0000";
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = backgroundColor;
+    console.log("Background color changed to " + backgroundColor);
+    alert("Background color changed to " + backgroundColor);
+}
+
+/*
+
+function changeHeaderColor() {
+    document.getElementById("header").style.color = headerColor;
+    console.log("Background color changed to " + headerColor);
+    alert("Background color changed to " + headerColor);
+}
+
+function changeParagraphColor() {
+    document.getElementById("paragraph").style.color = paragraphColor;
+    console.log("Background color changed to " + paragraphColor);
+    alert("Background color changed to " + paragraphColor);
+}
+ 
+*/
+
